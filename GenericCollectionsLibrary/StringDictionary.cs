a2523b521e4f3de276a83afcf329b1c78f28c405
Kenneth M Doud <kmdoud@gmail.com>
2019-02-26 09:32:17 -0500

    initial commit

---
+++ GenericCollectionsLibrary/StringDictionary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollectionsLibrary
+{
+    public class StringDictionary
+    {
+        private Dictionary<int, string> strings = new Dictionary<int, string>();
+
+        public void Add(int key, string data)
+        {
+            strings.Add(key, data);
+        }
+        public string Get(int key)
+        {
+            return strings[key];
+        }
+        public List<string> values()
+        {
+            return strings.Values.ToList();
+        }
+
+    }
+}
---
+++ TestGenericCollectionsLibrary/Program.cs
@@ -0,0 +1,44 @@
+﻿using GenericCollectionsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGenericCollectionsLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GenericCollections.About);
+
+            var stringDictionary = new StringDictionary();
+            stringDictionary.Add(7, "Irena");
+            stringDictionary.Add(4, "Sammy");
+            stringDictionary.Add(25, "Ben");
+            stringDictionary.Add(42, "Seth");
+            stringDictionary.Add(97, "Ken");
+            stringDictionary.Add(55, "Kellan");
+            stringDictionary.Add(3, "Chris");
+
+            Console.WriteLine($"The person with the fav nbr of 42 is {stringDictionary.Get(42)}");
+
+            foreach(var data in stringDictionary.values())
+            {
+                Console.WriteLine(data);
+            }
+
+
+            var strings = new StringList();
+            strings.Add("ABC");
+            strings.Add("DEF");
+            strings.Add("GHI");
+            strings.Add("Ab", "CD", "EF", "GH");
+            foreach(var str in strings)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
